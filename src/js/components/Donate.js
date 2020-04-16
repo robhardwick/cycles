1cf7c92fc9c919dffe59335e5fba6a645d4d6117
Rob Hardwick <robhardwick@gmail.com>
2020-04-16 21:33:52 +0100

    Update donation link

--- src/js/components/Donate.js
+++ src/js/components/Donate.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Donate = () => (
     <div className="donate">
-        <a href="https://modernnature.band" target="_blank">Like this? Donate here.</a>
+        <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=modernnatureuk%40gmail.com&amp;currency_code=GBP&amp;source=url" target="_blank">Friends &amp; Family</a>
     </div>
 );
